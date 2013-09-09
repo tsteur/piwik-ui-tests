4883aebca9ccffa4f690c1b018a59b2545e60716
Benaka Moorthi <benaka.moorthi@gmail.com>
2013-09-09 18:00:09 -0400

    Remove UI dir and make sure submodules are inited & updated.

--- getpiwik.sh
+++ getpiwik.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 if [ ! -f piwik_commit.txt ]; then
-    PIWIK_COMMIT=master
+    PIWIK_COMMIT=uitests
 else
     PIWIK_COMMIT=$(cat piwik_commit.txt)
 fi
 
 cd ..
 git clone --depth=50 "--branch=$PIWIK_COMMIT" git://github.com/piwik/piwik.git piwik
+
+cd piwik
+git submodule update --init --recursive
