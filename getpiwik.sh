ab4b6873ba89f616a0603a810f979387460d917f
Benaka Moorthi <benaka.moorthi@gmail.com>
2013-09-10 10:12:33 -0400

    Fix getpiwik.sh.

--- getpiwik.sh
+++ getpiwik.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
 if [ ! -f piwik_commit.txt ]; then
-    PIWIK_COMMIT=uitests
+    PIWIK_BRANCH=uitests
 else
-    PIWIK_COMMIT=$(cat piwik_commit.txt)
+    read -a lines -d '\n' < piwik_commit.txt
+
+    PIWIK_COMMIT="${lines[0]}"
+    PIWIK_BRANCH="${lines[1]}"
 fi
 
 cd ..
-git clone --depth=50 "--branch=$PIWIK_COMMIT" git://github.com/piwik/piwik.git piwik
+git clone --depth=50 "--branch=$PIWIK_BRANCH" git://github.com/piwik/piwik.git piwik
+
+if [ "$PIWIK_COMMIT" != "" ]; then
+    git checkout -qf "$PIWIK_COMMIT"
+fi
 
 cd piwik
 git submodule update --init --recursive
